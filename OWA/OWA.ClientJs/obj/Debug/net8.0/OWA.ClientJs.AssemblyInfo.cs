04d1bdd4d1b0bf63e151cbf4101c763809c919fa
Dariusz Ogórek <Dariusz.ogorek@advokat.at>
2025-01-28 16:25:18 +0100

    Added Custom client project

--- OWA/OWA.ClientJs/obj/Debug/net8.0/OWA.ClientJs.AssemblyInfo.cs
+++ OWA/OWA.ClientJs/obj/Debug/net8.0/OWA.ClientJs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OWA.ClientJs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4392235d7572d5d74aea957cd0845b5c4343f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8118cbfe1afe691b6128052ef17d6c41b8ab14f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OWA.ClientJs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OWA.ClientJs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
