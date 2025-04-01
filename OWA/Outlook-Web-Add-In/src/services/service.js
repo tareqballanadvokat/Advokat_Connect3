f418d1ef814c802cca1b45de64fb11ed3cc1fd5d
Dariusz Ogórek <Dariusz.ogorek@advokat.at>
2025-04-01 12:39:41 +0200

    unique Id of message

--- OWA/Outlook-Web-Add-In/src/services/service.js
+++ OWA/Outlook-Web-Add-In/src/services/service.js
@@ -11,34 +11,79 @@ Office.onReady((info) => {
  
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run2").onclick = run2;
+    document.getElementById("uniqueId").onclick = getUniqueId;
+    document.getElementById("uniqueId2").onclick = getUniqueId2;
+    document.getElementById("downloadId").onclick = getAsFileAsync;
   }
 });
-
-export async function run2() {
+export async function getUniqueId() {
   /**
    * Insert your Outlook code here
    */
 
   const item = Office.context.mailbox.item;
- 
-const options = Office.AsyncContextOptions = { asyncContext: { currentItem: item,  } };
+  const options = Office.AsyncContextOptions = { asyncContext: { currentItem: item,  } };
   Office.context.mailbox.item.getItemIdAsync(options, (result) => {
      const emailContent = result.value;
      debugger;
-    // const fileName = `${Office.context.mailbox.item.subject}.eml`;
-    const fileName = "service.eml";
-    const element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(emailContent));
-    element.setAttribute("download", `/Users/{user}/Desktop/${fileName}`);
-    element.style.display = "none";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+     const element = document.createElement("span");
+     element.innerHTML= encodeURIComponent(emailContent);
+
+     document.body.appendChild(element);
+ 
+  });  
+
+  Office.onReady(() => {
+    const item = Office.context.mailbox.item;
+  
+    item.getAllInternetHeadersAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        const headers = asyncResult.value;
+        const internetMessageId = headers.match(/Message-ID: (.+)/i);
+  
+        if (internetMessageId) {
+          console.log("Internet Message ID:", internetMessageId[1].trim());
+        } else {
+          console.warn("Missing Message-ID in header.");
+        }
+      } else {
+        console.error("Error:", asyncResult.error);
+      }
+    });
   });
+}
+export async function getUniqueId2() {
+  /**
+   * Insert your Outlook code here
+   */
+
+ 
+
+  Office.onReady(() => {
+    const item = Office.context.mailbox.item;
+  
+    item.getAllInternetHeadersAsync((asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+        const headers = asyncResult.value;
+        const internetMessageId = headers.match(/Message-ID: (.+)/i);
+  
+        if (internetMessageId) {
+          console.log("Internet Message ID:", internetMessageId[1].trim());
+        } else {
+          console.warn("Nie znaleziono Message-ID w nagłówkach.");
+        }
+      } else {
+        console.error("Błąd pobierania nagłówków:", asyncResult.error);
+      }
+    });
+  });
+}
 
 
-
+export async function getAsFileAsync() {
+ 
+  const item = Office.context.mailbox.item;
+  const options = Office.AsyncContextOptions = { asyncContext: { currentItem: item,  } };
   Office.context.mailbox.item.getAsFileAsync(options, (result) => {
      const emailContent = result.value;
      debugger;
@@ -52,19 +97,4 @@ const options = Office.AsyncContextOptions = { asyncContext: { currentItem: item
     element.click();
     document.body.removeChild(element);
   });
-
-
-  // Office.context.mailbox.item.body.getAsync("text", (result) => {
-  //   const emailContent = result.value;
-  //   const fileName = `${Office.context.mailbox.item.subject}.eml`;
-  //   const element = document.createElement("a");
-  //   element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(emailContent));
-  //   element.setAttribute("download", `/Users/{user}/Desktop/${fileName}`);
-  //   element.style.display = "none";
-  //   document.body.appendChild(element);
-  //   element.click();
-  //   document.body.removeChild(element);
-  // });
-
-
 }
