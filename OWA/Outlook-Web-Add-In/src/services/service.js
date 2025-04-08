0115dcec71d86a29ddf30be99856a7b6153d7d46
Dariusz Ogórek <Dariusz.ogorek@advokat.at>
2025-04-08 17:10:08 +0200

    Structure for partial download, ui fixes, etc

--- OWA/Outlook-Web-Add-In/src/services/service.js
+++ OWA/Outlook-Web-Add-In/src/services/service.js
@@ -9,96 +9,251 @@ Office.onReady(async (info) => {
  
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("caseStructureDownloaded").onclick = CaseDownloadStructure;
- 
+    document.getElementById("search-case-id").onclick = CaseSearchStructure;
   }
 });
 
 
-
-export async function CaseDownloadStructure() {
-// Define the API URL
-  //  const apiUrl = 'https://localhost:7231/WeatherForecast';
-    debugger;
-    var structureData ;
-    var itemsData ;
-    $.ajax({
-      type: 'GET',
-      url: 'https://localhost:7231/WeatherForecast/GetStructure',
-      contentType: "application/json",
-      //headers: { 'Authorization': 'Bearer ' + accessToken },
-      async: false,
-      success: function (data) {
-        console.log(data);
-        structureData = data;
-      },
-      error: function (textStatus, errorThrown) {
-        console.log(errorThrown);
-      }
-    });
-
-
-    $.ajax({
-      type: 'GET',
-      url: 'https://localhost:7231/WeatherForecast/GetMyItems',
-      contentType: "application/json",
-      //headers: { 'Authorization': 'Bearer ' + accessToken },
-      async: false,
-      success: function (data) {
-        console.log(data);
-        itemsData = data;
-        // CreateCaseStructure(data);
-      },
-      error: function (textStatus, errorThrown) {
-        console.log(errorThrown);
-      }
-
-    });
-
-    CreateCaseStructure(structureData,itemsData );
-}
-
  
-export async function CreateCaseStructure(data, items) 
+function initCaseStructure(initialData) 
 {
   var $rootElement = $("#caseStructure");
 
-  // Grupujemy elementy po rootId
-  var grouped = {};
-  data.forEach(item => {
-      const key = item.rootId ?? 'root'; // null → 'root'
-      if (!grouped[key]) grouped[key] = [];
-      grouped[key].push(item);
-  });
+  function buildTree(nodes, container) 
+  {
+      nodes.forEach(node => {
+          var $div = $("<div>", { class: "folder-row", id: "folder-" + node.id });
 
-  function buildTree(rootId, container) {
-      if (!grouped[rootId]) return;
-
-      grouped[rootId].forEach(node => {
-          var $div = $("<div>", { class: "folder", id: "folder-" + node.id });
-
-          // Title node
+          var $img = $("<img>", {
+            src: "assets/icon-16.png",
+            class: "folder-icon"
+           });
+          // Tytuł + toggle
           var $toggle = $("<span>", {
               class: "toggle",
-              text: "+ " + node.name
+              text: "+ ",
+              "data-node-id": node.id,
+              "data-last-node": node.lastNode
           });
-
+          var $name= $("<span>", {
+              class: "folder-name toggle",
+              text: node.name
+          });
           $div.append($toggle);
+          $div.append($img);
+          $div.append( $name);
 
-          // container for childrens
-          var $childContainer = $("<div>", { class: "children", css: { "margin-left": "20px", "display": "none" } });
-          $div.append($childContainer);
-
-          // OnClick
-          $toggle.on("click", function () {
-              $childContainer.toggle();
+          // Placeholder na dzieci
+          var $childContainer = $("<div>", {
+              class: "children",
+              css: { "margin-left": "20px", "display": "none" }
           });
 
+          $div.append($childContainer);
           container.append($div);
 
-          // Rekurencja – budujemy podfoldery
-          buildTree(node.id, $childContainer);
+          // Obsługa kliknięcia
+          $toggle.on("click", async function () {
+              const isVisible = $childContainer.is(":visible");
+              $childContainer.toggle();
+              $toggle.text((isVisible ? "+ " : "-  "));
+
+              // Ładowanie podstruktury tylko przy pierwszym kliknięciu
+              if (!isVisible && !$childContainer.data("loaded")) {
+                  const nodeId = $(this).data("node-id");
+                  const isLastNodeId = $(this).data("last-node");
+                  if (isLastNodeId)
+                    {
+                      // try {
+                      //   const subNodes = await createItemStructure(nodeId); // async load
+                      //   buildItemsTree(subNodes, $childContainer);
+                      //   $childContainer.data("loaded", true); // flag that it's loaded
+                      // } catch (error) {
+                      //     console.error("Błąd ładowania itemów:", error);
+                      // }
+                      
+                      loadItems(nodeId, $childContainer);
+                    }
+                    else 
+                    {
+                      try {
+                          const subNodes = await createCaseStructure(nodeId); // async load
+                          buildTree(subNodes, $childContainer);
+                          $childContainer.data("loaded", true); // flag that it's loaded
+                      } catch (error) {
+                          console.error("Błąd ładowania podstruktury:", error);
+                      }
+                  }
+              }
+          });
       });
   }
 
-  buildTree('root', $rootElement);
+
+
+  // Początkowe ładowanie root'ów
+  buildTree(initialData, $rootElement);
 }
+
+
+function buildItemsTree(nodes, container) {
+  nodes.forEach(node => {
+      var $div = $("<div>", { class: "file-tag", id: "file-" + node.id });
+
+      // Tytuł + toggle
+      var $toggle = $("<span>", {
+          class: "toggle",
+          text: node.name,
+          "data-node-id": node.id,
+          "data-last-node": node.lastNode,
+          "data-url": "url"
+      });
+
+      $div.append($toggle);
+
+      // Placeholder na dzieci
+      // var $childContainer = $("<div>", {
+      //     class: "children",
+      //     css: { "margin-left": "20px" }
+      // });
+
+      // $div.append($childContainer);
+      container.append($div);
+
+      // Obsługa kliknięcia
+      $toggle.on("click", async function () {
+          var url=  $(this).data("url");
+          console.log(url); 
+      });
+  });
+}
+
+
+
+async function loadItems(nodeId,  childContainer)
+{
+  try {
+    const subNodes = await createItemStructure(nodeId); // async load
+    buildItemsTree(subNodes, childContainer);
+    childContainer.data("loaded", true); // flag that it's loaded
+  } catch (error) {
+      console.error("Błąd ładowania itemów:", error);
+  }
+}
+export async function CaseDownloadStructure()
+{
+  const initialData = await createCaseStructure(0); // ładowanie root'ów
+  initCaseStructure(initialData);
+}
+
+export async function CaseSearchStructure()
+{
+  const initialData =  document.getElementById('text-search-input').value;
+  // searchCases(initialData);
+  searchCases(initialData)
+  .then(data => {
+    const $results = $("#results-container");
+    $results.empty(); // czyść stare dane
+
+    data.forEach(item => {
+      const $row = $("<div>", { class: "result-row" });
+
+      $row.append($("<div>").text(item.caseId));
+      $row.append($("<div>").text(item.causa));
+      $row.append($("<div>").html(`<button class="fav-btn">Add to favorite</button>`));
+
+      $results.append($row);
+    });
+  })
+  .catch(err => {
+    console.error("Błąd podczas wyszukiwania:", err);
+  });
+}
+function createItemStructure(nodeId) {
+  return fetch("https://localhost:7231/WeatherForecast/GetItemsByParentId?parentId=" + nodeId)
+      .then(res => res.json());
+}
+
+// function searchCases(searchQuery) {
+//       return fetch("https://localhost:7231/WeatherForecast/SearchCases", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+ 
+//       body: JSON.stringify({ query: searchQuery })
+//     })
+//     .then(res => res.json());
+
+ 
+// }
+
+
+function searchCases(searchQuery) {
+  return fetch("https://localhost:7231/WeatherForecast/SearchCases", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ query: searchQuery })
+  })
+    .then(res => res.json());
+}
+
+// $("#search-case-id").on("click", function () {
+//   const query = $("#text-search-input").val();
+//   if (!query) return;
+
+//   searchCases(query)
+//     .then(data => {
+//       const $results = $("#results-container");
+//       $results.empty(); // czyść stare dane
+
+//       data.forEach(item => {
+//         const $row = $("<div>", { class: "result-row" });
+
+//         $row.append($("<div>").text(item.caseId));
+//         $row.append($("<div>").text(item.causa));
+//         $row.append($("<div>").html(`<button class="fav-btn">Add to favorite</button>`));
+
+//         $results.append($row);
+//       });
+//     })
+//     .catch(err => {
+//       console.error("Błąd podczas wyszukiwania:", err);
+//     });
+// });
+
+
+function createCaseStructure(nodeId) {
+  return fetch("https://localhost:7231/WeatherForecast/GetStructureById?parentId=" + nodeId)
+      .then(res => res.json());
+}
+
+export async function GetItems(id) {
+  // Define the API URL
+    //  const apiUrl = 'https://localhost:7231/WeatherForecast';
+      debugger;
+      var itemsData ;
+ 
+      $.ajax({
+        type: 'GET',
+        url: 'https://localhost:7231/WeatherForecast/GetMyItems?',
+        contentType: "application/json",
+        //headers: { 'Authorization': 'Bearer ' + accessToken },
+        async: false,
+        success: function (data) {
+          console.log(data);
+          itemsData = data;
+          // CreateCaseStructure(data);
+        },
+        error: function (textStatus, errorThrown) {
+          console.log(errorThrown);
+        }
+  
+      });
+  
+       
+  }
+
+ 
--- OWA/OutlookAddIn.WebAPI/Controllers/WeatherForecastController.cs
+++ OWA/OutlookAddIn.WebAPI/Controllers/WeatherForecastController.cs
@@ -18,16 +18,16 @@ public class WeatherForecastController : ControllerBase
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
-        customTree.Add(new HierarchyTree { Id = 1, Name = "ADVOKAT", RootId = null });
-        customTree.Add(new HierarchyTree { Id = 2, Name = "Test", RootId = 1 });
-        customTree.Add(new HierarchyTree { Id = 3, Name = "Outlook", RootId = 1 });
+        customTree.Add(new HierarchyTree { Id = 1, Name = "ADVOKAT", RootId = 0 });
+        customTree.Add(new HierarchyTree { Id = 2, Name = "Test", RootId = 1, LastNode = true });
+        customTree.Add(new HierarchyTree { Id = 3, Name = "Outlook", RootId = 1, LastNode = false });
         customTree.Add(new HierarchyTree { Id = 4, Name = "Zusammenarbeit", RootId = 1 });
 
-        customTree.Add(new HierarchyTree { Id = 5, Name = "Briefe", RootId = 4 });
-        customTree.Add(new HierarchyTree { Id = 25, Name = "Briefe2", RootId = 4 });
+        customTree.Add(new HierarchyTree { Id = 5, Name = "Briefe", RootId = 4 , LastNode = true});
+        customTree.Add(new HierarchyTree { Id = 25, Name = "Briefe2", RootId = 4, LastNode = true });
 
-        customTree.Add(new HierarchyTree { Id = 6, Name = "Briefe", RootId = 3 });
-        customTree.Add(new HierarchyTree { Id = 7, Name = "Schriftsätze", RootId = 3 });
+        customTree.Add(new HierarchyTree { Id = 6, Name = "Briefe", RootId = 3, LastNode = true });
+        customTree.Add(new HierarchyTree { Id = 7, Name = "Schriftsätze", RootId = 3, LastNode = true });
 
         customItems.Add(new HierarchyItem { Id = 1, Name = "Test1.pdf", RootId = 4 });
         customItems.Add(new HierarchyItem { Id = 2, Name = "Test2.pdf", RootId = 4 });
@@ -42,20 +42,39 @@ public class WeatherForecastController : ControllerBase
         return new JsonResult(customTree);
     }
 
-
-    [HttpGet(Name = "GetMyItems")]
-    public ActionResult<HierarchyItem> GetMyItems()
+    [HttpGet(Name = "GetStructureById")]
+    public ActionResult<HierarchyTree> GetStructureById([FromQuery] int? parentId)
     {
-        return new JsonResult(customItems);
+      //  return new JsonResult(customTree);
+        return new JsonResult(customTree.Where(x => x.RootId == parentId).ToList());
+    }
+
+    [HttpGet(Name = "GetItemsByParentId")]
+    public ActionResult<HierarchyItem> GetItemsByParentId([FromQuery] int? parentId)
+    {
+        return new JsonResult(customItems.Where(x => x.RootId == parentId).ToList());
+      //  return new JsonResult(customItems);
+    }
+
+
+    [HttpPost(Name = "SearchCases")]
+    public ActionResult<HierarchyItem> SearchCases([FromBody] SearchRequest query)
+    {
+        //   return new JsonResult(customItems.Where(x => x.Name.Contains(query.Query)).ToList());
+       return new JsonResult(customItems);
     }
 
 }
-
+public class SearchRequest
+{
+    public string Query { get; set; }
+}
 public class HierarchyTree
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public int? RootId { get; set; }
+    public bool LastNode { get; set; }
 }
 
 public class HierarchyItem
@@ -63,5 +82,6 @@ public class HierarchyItem
     public int Id { get; set; }
     public int? RootId { get; set; }
     public string Name { get; set; }
+    public string Url { get; set; }
 
 }
