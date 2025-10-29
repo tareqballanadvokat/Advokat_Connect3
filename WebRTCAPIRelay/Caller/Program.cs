cd16ac0791c76c031715d635ad77a3c7f852f8b9
Martin Achenrainer <martin.achenrainer@advokat.at>
2025-10-29 14:02:36 +0100

    cahnged test caller and api to work again

--- WebRTCAPIRelay/Caller/Program.cs
+++ WebRTCAPIRelay/Caller/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
+using System;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
+using Advokat.WebRTC.Plugin.Chunking.DTOs;
+using Advokat.WebRTC.Plugin.DTOs;
 using WebRTCClient;
-using WebRTCAPIRelay.DTOs;
-using System.Security.Cryptography;
 
 namespace Caller
 {
@@ -12,16 +14,20 @@ namespace Caller
     {
         static WebRTCPeer UserAgent { get; set; }
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            _ = Task.Run(Run);
+            //_ = Task.Run(Run);
+
+            await Run();
 
             while (true)
             {
             };
         }
 
-        private async static void Run()
+        private static string[]? Response = null;
+
+        private async static Task Run()
         {
             string callerName = "macc";
             IPEndPoint? callerEndpoint = new IPEndPoint(Dns.GetHostAddresses(Dns.GetHostName()).LastOrDefault(), 8098);
@@ -71,24 +77,127 @@ namespace Caller
                         AllowTrailingCommas = true,
                         PropertyNameCaseInsensitive = true,
                         DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                        PropertyNamingPolicy = JsonNamingPolicy.KebabCaseLower
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                     });
 
                     Console.WriteLine(Encoding.UTF8.GetString(message));
                     return;
                 }
 
-                var response = JsonSerializer.Deserialize<WebRTCResponse>(message, new JsonSerializerOptions()
+                WebRTCChunkAck? ack = null;
+                try
                 {
-                    AllowTrailingCommas = true,
-                    PropertyNameCaseInsensitive = true,
-                    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                    PropertyNamingPolicy = JsonNamingPolicy.KebabCaseLower
-                });
+                    ack = JsonSerializer.Deserialize<WebRTCChunkAck>(message, new JsonSerializerOptions()
+                    {
+                        AllowTrailingCommas = true,
+                        PropertyNameCaseInsensitive = true,
+                        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
 
-                Console.WriteLine(Encoding.UTF8.GetString(message));
-                Console.WriteLine("---");
-                Console.WriteLine(Encoding.UTF8.GetString(response.Payload.Body));
+                    Console.WriteLine(Encoding.UTF8.GetString(message));
+                    Console.WriteLine("+++");
+                }
+                catch
+                {
+
+                }
+
+                WebRTCResponseChunk? response;
+                try
+                {
+                    response = JsonSerializer.Deserialize<WebRTCResponseChunk>(message, new JsonSerializerOptions()
+                    {
+                        AllowTrailingCommas = true,
+                        PropertyNameCaseInsensitive = true,
+                        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    //Console.WriteLine(Encoding.UTF8.GetString(message));
+                    Console.WriteLine($"totalChunks: {response.Payload.TotalChunks}; currentChunk: {response.Payload.CurrentChunk}");
+                    //Console.WriteLine("---");
+                    //Console.WriteLine("base64 string: ", response.Payload.Body);
+
+                    //Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(response.Payload.Body)));
+                }
+                catch
+                {
+                    if (ack == null)
+                    {
+                        throw;
+                    }
+
+                    return;
+                }
+
+                if (response == null)
+                {
+                    throw new Exception();
+                }
+
+                //string ackBody = $$"""
+                //{
+                //    "timestamp": {{((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds()}},
+                //    "chunk": {{((JsonElement)response.Payload).GetProperty("currentChunk").GetInt32()}}
+                //}
+                //""";
+                //string responseAck = $$"""
+                //{
+                //  "checksum": "{{Convert.ToBase64String(MD5.HashData(Encoding.UTF8.GetBytes(ackBody)))}}",
+                //  "id": "{{response.Id}}",
+                //  "body": {{ackBody}}
+                //}
+                //""";
+
+                //await sender.SendMessageToPeer(responseAck);
+
+                //if (Response == null)
+                //{
+                //    Response = new string[((JsonElement)response.Payload).GetProperty("totalChunks").GetInt32()];
+                //}
+
+                //Response[((JsonElement)response.Payload).GetProperty("currentChunk").GetInt32() - 1] = ((JsonElement?)response?.Payload)?.GetProperty("body").GetString();
+
+
+
+
+                string ackBody = $$"""
+                {
+                    "timestamp": {{((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds()}},
+                    "chunk": {{response.Payload.CurrentChunk}}
+                }
+                """;
+                string responseAck = $$"""
+                {
+                  "checksum": "{{Convert.ToBase64String(MD5.HashData(Encoding.UTF8.GetBytes(ackBody)))}}",
+                  "id": "{{response.Id}}",
+                  "body": {{ackBody}}
+                }
+                """;
+
+                await sender.SendMessageToPeer(responseAck);
+                Console.WriteLine($"confirmed: {response.Payload.CurrentChunk}");
+
+                if (Response == null)
+                {
+                    Response = new string[response.Payload.TotalChunks];
+                }
+
+                //Response[response.Payload.CurrentChunk - 1] = response.Payload?.Body;
+                Response[response.Payload.CurrentChunk - 1] = "yes";
+
+
+
+                if (Response.All(c => c != null))
+                {
+                    Console.WriteLine("ooo");
+                    //Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(string.Join("", Response))));
+                    Console.WriteLine("Done");
+
+                    Console.WriteLine("ooo");
+
+                    Response = null;
+                }
             };
 
             await UserAgent.Connect();
@@ -96,25 +205,61 @@ namespace Caller
 
         private async static Task SendRequest()
         {
-            string uri = "/test";
+            string guid = Guid.NewGuid().ToString();
+            string uri = "/akten";
+            //string uri = "/connect/token";
+            string method = "POST";
+            string headers = """
+                {
+                    "Content-Type":"application/json",
+                    "Accept":"application/json"
+                }
+                """;
             
             Console.WriteLine();
             Console.WriteLine($"Enter to send GET request to {uri}");
             Console.ReadLine();
 
-            string guid = "079175de-6b75-4582-a2f8-64e776c60c44";
-            string guidChecksum = Encoding.UTF8.GetString(MD5.HashData(Encoding.UTF8.GetBytes(guid)).Take(4).ToArray());
+            byte[] calculatedHash = MD5.HashData(Encoding.UTF8.GetBytes(guid));
+;
+            StringBuilder calculatedChecksum = new StringBuilder();
+            foreach (byte h in calculatedHash[..2])
+            {
+                calculatedChecksum.Append(h.ToString("x2"));
+            }
 
-            string payload = """
+            string guidChecksum = calculatedChecksum.ToString();
+
+            List<string> chunks = [
+                "eyJncmFudF90e",
+                "XBlIjoicGFzc3",
+                "dvcmQiLCJjbGllbnRfaWQiOiJhZH",
+                "Zva2F0LmNsaWVudC5",
+                "3ZWIiLCJjbGllbnRfc2VjcmV0IjoiYWR2",
+                "b2thdCIsInVzZXJu",
+                "YW1lIjoiSkNIIiwicGFzc3dvc",
+                "mQiO",
+                "iIif",
+                "Q=="
+            ];
+
+            for (int i = 1; i <= chunks.Count; i++)
+            {
+                await SendMessage(guid, guidChecksum, method, uri, headers, chunks.Count, i, chunks[i - 1]);
+            }
+        }
+
+        private async static Task SendMessage(string guid, string guidChecksum, string method, string uri, string headers, int totalChunks, int currentChunk, string body)
+        {
+            string payload = $$"""
                 {
-                    "timestamp": 1755511740818,
-                    "method": "GET",
-                    "uri": "/akten",
-                    "Headers": {
-                        "Content-Type":"application/json",
-                        "Accept":"application/json"
-                    },
-                    "body": "ewogICAgICAgICAgICAgICAgICAgICAgICAiQWt0SWQiOiAxMiwKICAgICAgICAgICAgICAgICAgICAgICAgIkFLdXJ6TGlrZSI6ICJhYmMiLAogICAgICAgICAgICAgICAgICAgICAgICAiQ291bnQiOiA0LAogICAgICAgICAgICAgICAgICAgICAgICAiTnVyRmF2b3JpdGVuIjogdHJ1ZSwKICAgICAgICAgICAgICAgICAgICAgICAgIldpdGhDYXVzYSI6IGZhbHNlCiAgICAgICAgICAgICAgICAgICAgfQ=="
+                    "timestamp": {{((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds()}},
+                    "totalChunks": {{totalChunks}},
+                    "currentChunk": {{currentChunk}},
+                    "method": "{{method}}",
+                    "uri": "{{uri}}",
+                    "Headers": {{headers}},
+                    "body": "{{body}}"
                   }
                 """;
 
@@ -125,7 +270,8 @@ namespace Caller
                 {
                   "checksum": "{{checksumString}}",
                   "id": "{{guid}}{{guidChecksum}}",
-                  "request": {{ payload}}
+                  "messageType": "testmessagetype",
+                  "request": {{payload}}
                 }
                 """;
 
@@ -133,3 +279,23 @@ namespace Caller
         }
     }
 }
+
+//requestString = """
+//    {
+//        "checksum": "/qiexFpUhOxADHRr6rbToA==",
+//        "id": "f87fd296-e36a-48f6-8e19-318cca0a4aceba0c",
+//        "isMultipart": false,
+//        "request": {
+//            "timestamp": 1758092677449,
+//            "totalChunks": 0,
+//            "currentChunk": 0,
+//            "method": "POST",
+//            "uri": "connect/token",
+//            "headers": {
+//                "Content-Type": "application/json",
+//                "Accept": "application/json"
+//            },
+//            "body": "eyJncmFudF90eXBlIjoicGFzc3dvcmQiLCJjbGllbnRfaWQiOiJhZHZva2F0LmNsaWVudC53ZWIiLCJ jbGllbnRfc2VjcmV0IjoiYWR2b2thdCIsInVzZXJuYW1lIjoiSkNIIiwicGFzc3dvcmQiOiIifQ=="
+//        }
+//    }
+//    """;
