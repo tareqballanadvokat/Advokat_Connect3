9549e1a2b6340849d03672b39b209500d9fcf257
Martin Achenrainer <martin.achenrainer@advokat.at>
2026-03-12 13:37:41 +0100

    MAC AKP-656 Moved signaling server to Advokat repo

--- OWA/SIPSignalingServer/Utils/CustomEventArgs/FailedRegistrationEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using SIPSignalingServer.Models;
-
-namespace SIPSignalingServer.Utils.CustomEventArgs
-{
-    public class FailedRegistrationEventArgs : FailureEventArgs
-    {
-        public SIPRegistration? Registration { get; set; }
-    }
-}
